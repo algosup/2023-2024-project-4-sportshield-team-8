bb1fc396276347f54a4b399d79a3e32e04bac280
Thomas Planchard <91249646+thomas-planchard@users.noreply.github.com>
2024-03-15 16:04:14 +0100

    Update Refactor.ino
    Problem to initialize AT command

--- src/Refactor/Refactor.ino
+++ src/Refactor/Refactor.ino
@@ -49,6 +49,15 @@ bool MotionBig = false;
 bool MotionSmall = false;
 bool MotionDetect = false;
 
+// SIM800L GSM 2G Module
+UART Serial2(D0, D1, NC, NC);
+#define SIM800_RST_PIN A5
+#define SIM800_DTR_PIN A5
+#define TIME_OUT_MS_BLE_ACT 5000  // 300s 300000 times in ms or bluetooth enabled to allow connection following motion detection. (value 5000 for 5s tests)
+SIM800L* sim800l;
+bool send_position = false;
+bool send_move = false;
+
 // Buzzer
 const int buzzerPin = D2;
 void PulseBuzzer(int repetitions, unsigned long durationOn, unsigned long durationOff);
@@ -117,7 +126,7 @@ void setup() {
     Serial.print(F("Starting ITimer OK, millis() = "));
     Serial.println(millis());
   }
-  ISR_Timer.setInterval(TIMER_INTERVAL_120S, GPS_ISR);
+  ISR_Timer.setInterval(TIMER_INTERVAL_120S, TimerHandler);
 
 
   ble_setup();
@@ -153,23 +162,23 @@ void setup() {
 
 void loop(){
 
-    MotionData = getMogtionData();
+    MotionData = getMotionData();
     RotationData = getRotationData();
     static String message = "";
     
-    if (Serial.availible() > 0){
+    if (Serial.available() > 0){
         char c = Serial.read();
         if (c == '\n' || c == '\r'){
             message.trim();
-            if (message.equalsIgnoreCase == "shutdown"){
+            if (message.equalsIgnoreCase("shutdown")){
                 Serial.println("Shutting down");
                 shutdown();
-            } else if (message == "unlock"){
+            } else if (message.equalsIgnoreCase("unlock")){
                 Serial.println("Unlocking");
                 unlock();
-            } else if (message == "lock"){
+            } else if (message.equalsIgnoreCase("lock")){
                 lock();
-            } else if (message == "battery"){
+            } else if (message.equalsIgnoreCase("battery")){
                 Serial.print("V Bat: ");
                 Serial.println(getBatteryVoltage());
                 Serial.print("Battery Level: ");
@@ -183,7 +192,58 @@ void loop(){
         }
     }
 
-    
+    if (Config.isActivate){
+        if (MotionData > SmallMT){
+            MotionDetect = true;
+            MotionSmall = true;
+            MotionBig = true;
+            tim_connec = millis();
+        }
+        if (RotationData > SmallRT){
+            MotionDetect = true;
+            MotionSmall = true;
+            MotionBig = true;
+            tim_connec = millis();
+        }
+    }
+
+    if (MotionDetect){
+        if (MotionData < SmallMT){
+            MotionSmall = false;
+        }
+        if (RotationData < SmallRT){
+            MotionSmall = false;
+        }
+        if (MotionData < BigMT){
+            MotionBig = false;
+        }
+        if (RotationData < BigRT){
+            MotionBig = false;
+        }
+        if (MotionSmall == false && MotionBig == false){
+            MotionDetect = false;
+        }
+    }
+
+    if (MotionDetect && BLE_activated){
+        if (millis() - tim_connec > 30000){
+            BLE_activated = false;
+            Serial.println("BLE deactivated");
+        }
+    }   
+
+    if (millis() - StartCoolDown > 30000){
+        StartCoolDown = millis();
+        if (MotionDetect){
+            PulseBuzzer(1, 100, 100);
+        }
+    }
+
+   
+
+
+
+
 
 }
 
@@ -241,37 +301,44 @@ void loop(){
 
  }
 
- void imu_setup(void){
-    if (!imu.init()) {
-    Serial.println("Failed to detect and initialize IMU!");
-    while (1){
-        digitalWrite(LEDR, HIGH);
-        delay(500);
-        digitalWrite(LEDR, LOW);
-        delay(500);
-        }
-    }
-    Serial.println("IMU started");
-    imu.enableDefault();
-    imu.writeAccelODR(LSM6DS3::ODR_52Hz);
-    imu.writeGyroODR(LSM6DS3::ODR_52Hz);
-    Serial.println("IMU initialized");
- } 
+void imu_setup(void) {
+  if (imu.begin() != 0) {
+    Serial.println("Device error");
+  } else {
+    Serial.println("Accelerometer launched");
+  }
+}
 
- void sim_setup(void){
-    while (!sim800l->init()) {
-    Serial.println("Failed to detect and initialize SIM800L!");
-    while (1){
-        digitalWrite(LEDR, HIGH);
-        delay(500);
-        digitalWrite(LEDR, LOW);
-        delay(500);
-        }
+void sim_setup(void){
+    while (!sim800l->isReady()) {
+        Serial.println(F("Problem to initialize AT command, retry in 1 sec"));
+        digitalWrite(LEDR, !digitalRead(LEDR));
+        delay(1000);
     }
-    Serial.println("SIM800L started");
-    sim800l->enableNetLight();
-    Serial.println("SIM800L initialized");
-}   
+    sim800l->enableEchoMode();
+    sim800l->setupGPRS("iot.1nce.net");
+
+    uint8_t signal = sim800l->getSignal();
+    while (signal <= 0) {
+        delay(1000);
+        signal = sim800l->getSignal();
+        Serial.println(F("Waiting signal..."));
+    }
+    Serial.println(String(signal));
+    NetworkRegistration network = sim800l->getRegistrationStatus();
+    // while (network != REGISTERED_HOME && network != REGISTERED_ROAMING) {
+    //   delay(1000);
+    //   network = sim800l->getRegistrationStatus();
+    //   Serial.print(network + " ");
+    //   Serial.println(F("Problem to register, retry in 1 sec"));
+    //   digitalWrite(LEDG, !digitalRead(LEDG));
+    // }
+    delay(50);
+    sim800l->setPowerMode(MINIMUM);      // set minimum functionnality mode
+    digitalWrite(SIM800_DTR_PIN, HIGH);  // put in sleep mode
+}
+
+
 
 void Temps(void) {
   unsigned long millisPassed = millis();
@@ -315,6 +382,100 @@ float getRotationData() {
   return fabs(RotationDataerence);  //returns a value always positive
 }
 
+void TimerHandler() {
+  ISR_Timer.run();
+}
+
+void onConnect(BLEDevice central) {
+  Serial.print("Connected to ");
+  Serial.println(central.address());
+  Serial.println(BLE.address());
+  digitalWrite(LEDB, LOW);
+}
+
+void onDisconnect(BLEDevice central) {
+  Serial.print(F("Disconnected from central: "));
+  Serial.println(central.address());
+  isAuthenticate = false;
+  digitalWrite(LEDB, HIGH);
+}
+
+
+void onWriteName(BLEDevice central, BLECharacteristic characteristic) {
+  if (isAuthenticate) {
+    Config.Name = NameCharacteristic.value();
+    String value = NameCharacteristic.value();
+    Serial.print("Written name : ");
+    Serial.println(value);
+  } else {
+    NameCharacteristic.writeValue("\n");
+  }
+}
+
+
+void onReadName(BLEDevice central, BLECharacteristic characteristic) {
+  if (isAuthenticate) {
+    NameCharacteristic.writeValue(Config.Name);
+  } else {
+    NameCharacteristic.writeValue("\n");
+  }
+
+}
+
+void onWriteActivation(BLEDevice central, BLECharacteristic characteristic) {
+  if (isAuthenticate) {
+    Config.isActivate = ActivationCharacteristic.value();
+    bool value = ActivationCharacteristic.value();
+    Serial.print("Written activation : ");
+    Serial.println(value);
+  } else {
+    ActivationCharacteristic.writeValue(false);
+  }
+}
+
+void onReadActivation(BLEDevice central, BLECharacteristic characteristic) {
+  if (isAuthenticate) {
+    ActivationCharacteristic.writeValue(Config.isActivate);
+  } else {
+    ActivationCharacteristic.writeValue(false);
+  }
+}
+
+void onWriteUnlock(BLEDevice central, BLECharacteristic characteristic) {
+  if (isAuthenticate) {
+    bool value = UnlockCharacteristic.value();
+    Serial.print("Written unlock : ");
+    Serial.println(value);
+    if (value) {
+      unlock();
+    }
+  } else {
+    UnlockCharacteristic.writeValue(false);
+  }
+}
+
+void onWritePassword(BLEDevice central, BLECharacteristic characteristic) {
+  const int motDePasseAttendu = 1234;
+  short int value = PasswordCharacteristic.value();
+  Conversion(value);
+  isAuthenticate = (value == motDePasseAttendu);
+  Serial.println(isAuthenticate ? "successful authentication" : "wrong password");
+}
+
+
+void Conversion(short int value) {
+  int i = 0;
+  int j = 0;
+  int tab[4];
+  while (value > 0) {
+    tab[i] = value % 10;
+    value = value / 10;
+    i++;
+  }
+  for (j = 0; j < i; j++) {
+    Config.pin = Config.pin + tab[j] * pow(10, j);
+  }
+}
 
 
 
@@ -363,9 +524,30 @@ void lock(){
 float getBatteryVoltage(){
   digitalWrite(VBAT_ENABLE, HIGH);
   delay(100);
-  float voltage = analogRead(A7) * 0.003363075;
+  float voltage = analogRead(PIN_VBAT) * 0.003363075;
   digitalWrite(VBAT_ENABLE, LOW);
   return voltage;
+}
+
+
+// getBatteryLevel : Get the battery level
+float getBatteryLevel() {
+  const float minTension = 3.7;
+  const float maxTension = 4.2;
+  float battery = analogRead(PIN_VBAT);
+
+  int minValue = (1023 * minTension) / 5;
+  int maxValue = (1023 * maxTension) / 5;
+
+  battery = ((battery - minValue) / (maxValue - minValue)) * 100;
+
+  if (battery > 100)
+    battery = 100;
+
+  else if (battery < 0)
+    battery = 0;
+  
+  return battery;
 }
 
 // pulse : Make the buzzer pulse
