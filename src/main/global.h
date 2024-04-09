8652aa9a6880dc24b223e44c5be4eb3981ce7957
Thomas Planchard <91249646+thomas-planchard@users.noreply.github.com>
2024-04-09 15:38:27 +0200

    Update global

--- src/main/global.h
+++ src/main/global.h
@@ -5,58 +5,35 @@
 
 #ifndef GLOBALS_H
 #define GLOBALS_H
-
-#include "NRF52_MBED_TimerInterrupt.h"
-#include "NRF52_MBED_ISR_Timer.h"
-#include <ArduinoBLE.h>
-#include "struct.h"
-#include <LSM6DS3.h>
-#include <Wire.h>
-#include <Adafruit_GPS.h>
-#include "SIM800L.h"
-
-// Timer settings
 #define TIMER_INTERRUPT_DEBUG 0
 #define _TIMERINTERRUPT_LOGLEVEL_ 0
-#define HW_TIMER_INTERVAL_MS 1000
-#define TIMER_INTERVAL_120S 120000L
+#include "NRF52_MBED_TimerInterrupt.h"
+#include "NRF52_MBED_ISR_Timer.h"
+#include <Arduino.h>
+// BLE
+#include <ArduinoBLE.h>
+#include "struct.h"
+// IMU
+#include <LSM6DS3.h>
+#include <Wire.h>
+//GPS
+#include <Adafruit_GPS.h>
+//SIM
+#include "SIM800L.h"
 
-// GPS Wakeup Pin
-#define GPS_WKUP_PIN D8
-
-// SIM800L Pins and Timeouts
-#define SIM800_RST_PIN A5
-#define SIM800_DTR_PIN A5
-#define TIME_OUT_MS_BLE_ACT 5000  // Timeout for BLE activity
-
-// Buzzer Pin
-#define BUZZER_PIN D2
-
-// Electro-magnet Pin
-#define AIMANT_PIN D3
-
-// Battery Voltage Enable Pin
-#define VBAT_ENABLE 14
-
-// Battery Voltage Thresholds
-#define FULL_CHARGE 3.65
-#define LOW_BATTERY 3.475
-#define LOCKED_BATTERY 3.4
-
-// Motion Thresholds
-#define SMALL_MOTION_THRESHOLD 40.0
-#define BIG_MOTION_THRESHOLD 170.0
-#define SMALL_ROTATION_THRESHOLD 40.0
-#define BIG_ROTATION_THRESHOLD 145.0
-
-// Global Configuration
+//---------------- GLOBAL VARIABLES -----------------------------
 myConfig Config;
 bool isAuthenticate = false;
-
-// BLE Global Variables
-BLEService PasswordService("19B10000-E8F2-537E-4F6C-D104768A1213");
+// Timer
+#define HW_TIMER_INTERVAL_MS 1
+NRF52_MBED_Timer ITimer(NRF_TIMER_3);
+NRF52_MBED_ISRTimer ISR_Timer;
+#define TIMER_INTERVAL_120S 120000L
+// BLE
+BLEService PasswordService("19B10000-E8F2-537E-4F6C-D104768A1213");  // Bluetooth® Low Energy Service
 BLEService ConfigService("19B10000-E8F2-537E-4F6C-D104768A1214");
-BLEStringCharacteristic PasswordCharacteristic("19B10000-E8F2-537E-4F6C-D104768A1213", BLEWrite, 32);
+
+BLEShortCharacteristic PasswordCharacteristic("19B10000-E8F2-537E-4F6C-D104768A1213", BLEWrite);  // Bluetooth® Low Energy Characteristic
 BLEStringCharacteristic NameCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite, 20);
 BLEStringCharacteristic MACCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1217", BLERead, 20);
 BLEBooleanCharacteristic ActivationCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1215", BLERead | BLEWrite);
@@ -68,28 +45,55 @@ BLEDescriptor ActivationDescriptor("2901", "Activation");
 BLEDescriptor UnlockDescriptor("2901", "Unlock");
 BLEDescriptor MACDescriptor("2901", "MAC Address");
 
-BLEDevice userDevice;  // Device connected to BLE
+bool BLE_activated = true;  //true if the bluetooth is activated
+uint32_t tim_connec = 0;    // time in ms or we start to activate the bluetooth following a detection of movement
 
-// IMU (LSM6DS3) Global Variables
-LSM6DS3 imu(I2C_MODE, 0x6A);
 
-// GPS Global Variables
+//IMU : LSM6DS3
+LSM6DS3 imu(I2C_MODE, 0x6A);  //I2C device address 0x6A
+uint32_t timer_imu = millis();
+bool MotionBig = false;
+bool MotionSmall = false;
+bool MotionDetect = false;
+
+// GPS PA1010D
 Adafruit_GPS GPS(&Serial1);
+bool start_gps = false;
+bool position_acquired = false;
+uint32_t timer = millis();
+#define GPS_WKUP_PIN D8
 
-// SIM800L Global Variables
+// SIM800L GSM 2G Module
 UART Serial2(D0, D1, NC, NC);
+#define SIM800_RST_PIN A5
+#define SIM800_DTR_PIN A5
+#define TIME_OUT_MS_BLE_ACT 5000  // 300s 300000 times in ms or bluetooth enabled to allow connection following motion detection. (value 5000 for 5s tests)
 SIM800L* sim800l;
+bool send_position = false;
+bool send_move = false;
 
-// Battery and Movement Detection Variables
-bool isLowBattery = false;
-bool isLockedBattery = false;
+// Buzzer
+const int buzzerPin = D2;
+unsigned long previousMillis = 0;
+int currentRep = 0;
+
+//Electroaimant
+const int aimantPin = D3;
+
+// Set a threshold to determine a "small" or "big" movement
+
+float SmallMT = 20.0;  //     SmallMotionThreshold
+float BigMT = 150.0;   //    BigMotionThreshold
+
+float SmallRT = 20.0;  //     SmallRotationThreshold
+float BigRT = 125.0;   //     BigRotationThreshold
+
+//batterie
+#define VBAT_ENABLE 14
+
 float MotionData;
 float RotationData;
-float lastLatitude = 0.0;
-float lastLongitude = 0.0;
 
-// Timer Objects
-NRF52_MBED_Timer ITimer(NRF_TIMER_3);
-NRF52_MBED_ISRTimer ISR_Timer;
+unsigned long StartCoolDown = 0;  //check point for millis aided cooldown
 
 #endif // GLOBALS_H
